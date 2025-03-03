ed12c00c912f587377607775268509a7fec3fb74
c0ral1ne <c0ralin3j0nesz@gmail.com>
2025-03-02 18:29:03 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,33 +32,20 @@ sudo apt install -y \
     zsh \
     ripgrep \
     python-is-python3 \
-    kitty \
     tzdata --fix-missing && \
     sudo rm -rf /var/lib/apt/list/*
 
 
 sudo gem install one_gadget seccomp-tools && sudo rm -rf /var/lib/gems/2.*/cache/*
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
-sudo apt-get install zsh-syntax-highlighting -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 python3 -m pip install -U pip && \
 python3 -m pip install --no-cache-dir \
-    pwntools \
-    ropgadget \
-    ropper
+    pwntools
 
 git clone --depth 1 https://github.com/pwndbg/pwndbg ~/pwndbg && \
 cd ~/pwndbg && chmod +x setup.sh && ./setup.sh
-
-git clone --depth 1 https://github.com/scwuaptx/Pwngdb.git ~/Pwngdb && \
-    cd ~/Pwngdb/pwndbg && \
-    cp pwngdb.py ~/pwndbg/pwndbg/pwngdb.py && \
-    cp angelheap.py ~/pwndbg/pwndbg/angelheap.py && \
-    cp commands/pwngdb.py ~/pwndbg/pwndbg/commands/pwngdb.py && \
-    cp commands/angelheap.py ~/pwndbg/pwndbg/commands/angelheap.py && \
-    sed -i -e '/config_mod.init_params()/a import pwndbg.commands.pwngdb' ~/pwndbg/pwndbg/__init__.py && \
-    sed -i -e '/config_mod.init_params()/a import pwndbg.commands.angelheap' ~/pwndbg/pwndbg/__init__.py
 
 # Install pwninit
 curl https://sh.rustup.rs -sSf | sh -s -- -y && \
@@ -66,9 +53,3 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y && \
     cargo install pwninit
 
 sudo chsh -s $(which zsh)
-
-cp /etc/zsh/newuser.zshrc.recommended ~/.zshrc
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-echo "source $(realpath ./scripts/pwnshell.sh)" >> ~/.zshrc
-
-source ~/.zshrc
